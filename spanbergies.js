bd20f3c876486d1a4a7e6316303b496667a60110
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-11-15 23:29:32 -0500

    initial commit

---
+++ spanbergies.js
@@ -0,0 +1,132 @@
+const awards = [
+  {
+    name_first: 'Adam',
+    name_last:  'Spanberger',
+    award:      'Best sign putter-upper and billboard believer',
+  },
+  {
+    name_first: 'Dana',
+    name_last:  'Bye',
+    award:      'Best removal of an undercover operative from office',
+  },
+  {
+    name_first: 'Karen',
+    name_last:  'Mask',
+    award:      'Best creation of long term morale building tool',
+  },
+  {
+    name_first: 'Max',
+    name_last:  'Heyworth',
+    award:      'Best on Slack',
+  },
+  {
+    name_first: 'Justin',
+    name_last:  'Jones',
+    award:      'Best late night whiskey provider',
+  },
+  {
+    name_first: 'Bettina',
+    name_last:  'Weiss',
+    award:      'Best brand ambassador for washable shoes made from recycled plastic who can give you a $20 coupon',
+  },
+  {
+    name_first: 'Sam Louis',
+    name_last:  'Taylor',
+    award:      'Best dancing by the coffee maker',
+  },
+  {
+    name_first: 'MacKenzie',
+    name_last:  'Heidelmark',
+    award:      'Best spying on a staffer while they were pretending to work at home ',
+  },
+  {
+    name_first: 'Kayla',
+    name_last:  'Enoch',
+    award:      'Best lifting kids upside down',
+  },
+  {
+    name_first: 'Wynne',
+    name_last:  'Graham',
+    award:      'Best hiding of birthday for selfless reasons',
+  },
+  {
+    name_first: 'Zoe',
+    name_last:  'Kozlowski',
+    award:      'Best putting up with food shit ',
+  },
+  {
+    name_first: 'Adriane',
+    name_last:  'Vaughn',
+    award:      'Best sassy faces',
+  },
+  {
+    name_first: 'Emma',
+    name_last:  'Carl',
+    award:      'Best vodka choice',
+  },
+  {
+    name_first: 'Whitney',
+    name_last:  'Holt',
+    award:      'Best job not fucking up a friend online the day after Election Day ',
+  },
+  {
+    name_first: 'Kristi',
+    name_last:  'Black',
+    award:      'Best coffee order',
+  },
+  {
+    name_first: 'Michael',
+    name_last:  'Phelan',
+    award:      'Best deflection of a tracker',
+  },
+  {
+    name_first: 'Eliza',
+    name_last:  'Wood',
+    award:      'Best job listening to Republicans for hours without bleeding from your ears',
+  },
+  {
+    name_first: 'Mary Jo',
+    name_last:  'Sheeley',
+    award:      'Best self care basket',
+  },
+  {
+    name_first: 'Nick',
+    name_last:  'Stevens',
+    award:      'Best beard',
+  },
+  {
+    name_first: 'Pat',
+    name_last:  'Dolan',
+    award:      'Best at avoiding photo',
+  },
+  {
+    name_first: 'Dale',
+    name_last:  'Wolf',
+    award:      'Best Westbury Pharmacy customer',
+  },
+  {
+    name_first: 'Jin',
+    name_last:  'Sheeley',
+    award:      'Best dog mom',
+  },
+  {
+    name_first: 'Rusty',
+    name_last:  'Tutton',
+    award:      'Best pinch hitter on the field team',
+  },
+  {
+    name_first: 'Aateeb',
+    name_last:  'Khan',
+    award:      'Best moving to San Francisco without telling anyone',
+  },
+  {
+    name_first: 'Michael',
+    name_last:  'Keegan',
+    award:      'Best positive attitude',
+  },
+  {
+    name_first: 'Abigail',
+    name_last:  'Spanberger',
+    award:      'Best British accent in French',
+  },
+]

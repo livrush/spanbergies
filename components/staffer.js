bcdec752395e592a59a3fda673f109ca9d7bdeeb
Liv <w.phrush@gmail.com>
2018-11-18 21:05:58 -0500

    add staffer component

---
+++ components/staffer.js
@@ -0,0 +1,16 @@
+app.component('staffer', {
+  bindings: {
+    staffer: '<',
+  },
+  controller: function t() {
+    const face = this;
+    console.log(face);
+    face.$onInit = function () {
+      const first = face.staffer.nameFirst.split(' ').join('-').toLowerCase();
+      const last = face.staffer.nameLast.toLowerCase();
+      face.url = '/assets/staff-photos/' + [first, last].join('-') + '.png';
+    };
+  },
+  controllerAs: 'face',
+  templateUrl: '/components/staffer-face.html',
+});

26b6f2a77448f4dbd792ebce8d7fee61af344d6b
Liv <w.phrush@gmail.com>
2018-11-18 20:23:32 -0500

    clean

--- components/toc.js
+++ components/toc.js
@@ -4,7 +4,6 @@ app.component('toc', {
   },
   controller: function t() {
     const toc = this;
-    console.log(toc);
   },
   controllerAs: 'toc',
   templateUrl: '/components/toc.html',
@@ -21,10 +20,8 @@ app.component('staffer', {
       const first = staffer.first.split(' ').join('-').toLowerCase();
       const last = staffer.last.toLowerCase();
       staffer.url = '/assets/staff-photos/' + [first, last].join('-') + '.png';
-      console.log(staffer.url);
     };
   },
   controllerAs: 'staffer',
-  // template: `<div class="staffer">{{staffer.first}}</div>`,
   template: `<div class="toc-staffer"><img ng-src="{{staffer.url}}" /></div>`,
 });

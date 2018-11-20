eb9948516c84be3f0ed6ed94cc8b2ce643954ec9
Liv <w.phrush@gmail.com>
2018-11-20 12:20:50 -0800

    update seal url creation

--- components/staffer-face.js
+++ components/staffer-face.js
@@ -8,7 +8,8 @@ app.component('face', {
     face.$onInit = function () {
       const first = face.staffer.nameFirst.split(' ').join('-').toLowerCase();
       const last = face.staffer.nameLast.toLowerCase();
-      face.url = './assets/staff-photos/' + [first, last].join('-') + '.png';
+      face.url = `./assets/staff-photos/${[first, last].join('-')}.png`;
+      face.seal = `./assets/seal-${face.theme}.svg`;
     };
   },
   controllerAs: 'face',

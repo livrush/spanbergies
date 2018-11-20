9863f0db7679ca6b51efdcac8ea7077bdf8e7263
Liv <w.phrush@gmail.com>
2018-11-19 23:23:30 -0800

    add icons component

---
+++ components/icons.js
@@ -0,0 +1,10 @@
+app.component('icons', {
+  bindings: {
+    icons: '<',
+  },
+  controller: function t() {
+    const icons = this;
+  },
+  controllerAs: 'icons',
+  templateUrl: '/components/icons.html',
+});
